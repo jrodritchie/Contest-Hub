98086abc20e12a8598d18fea3c6ef2d67d69ed89
jrodritchie <jrod.ritchie@gmail.com>
2026-09-19 15:34:30 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,50 +1,7 @@
-const CACHE = "contest-hub-v2";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./contests.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/icon-512-maskable.png"
-];
-
 self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE)
-          .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", event => {
-  const url = new URL(event.request.url);
-
-  if (url.pathname.endsWith("/contests.json")) {
-    event.respondWith(
-      fetch(event.request, { cache: "no-store" })
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE).then(cache => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => caches.match(event.request))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
+  event.waitUntil(self.clients.claim());
 });
